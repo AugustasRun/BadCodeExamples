ddf380e71e8465ee1e549b92cb694c8cb5df484a
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-04-13 19:34:19 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,20 +10,9 @@ namespace ConsoleApp1
             UnsafeQuery("test", "test", "test");
         }
 
-        public static object UnsafeQuery(string connection, string name, string password)
+        public static void UnsafeQuery(string connection, string name, string password)
         {
-            SqlConnection someConnection = new SqlConnection(connection);
-            SqlCommand someCommand = new SqlCommand();
-            someCommand.Connection = someConnection;
-
-            someCommand.CommandText = "SELECT AccountNumber FROM Users " +
-               "WHERE Username='" + name +
-               "' AND Password='" + password + "'";
-
-            someConnection.Open();
-            object accountNumber = someCommand.ExecuteScalar();
-            someConnection.Close();
-            return accountNumber;
+            var hashAlg = SHA1.Create();
         }
     }
 }
