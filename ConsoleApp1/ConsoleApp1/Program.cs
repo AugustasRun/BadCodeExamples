188fff72d93baf4bfee7548df7badbcc7ad680ff
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-04-19 15:27:21 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,17 +4,12 @@
     {
         static async Task Main(string[] args)
         {
-
         }
-    }
 
-    class C
-    {
-        public static void M(IReadOnlyList<string> list)
+        public static string Read(string path)
         {
-            Console.Write(list.First());
-            Console.Write(list.Last());
-            Console.Write(list.Count());
+            var reader = new StreamReader(path);
+            return reader.ReadToEnd();
         }
     }
 }
