114931f4af230b0607afc76c7d4bdd6434ef8ba5
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-04-19 20:05:36 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,6 +17,7 @@ namespace ConsoleApp1
         public ExampleClass()
         {
             Settings = new JsonSerializerSettings();
+            Settings.TypeNameHandling = TypeNameHandling.All;
         }
     }
 }
