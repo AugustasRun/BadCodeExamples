e0b1058da2fdb2e2f14483e4ddde2e2e194ec752
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-04-19 19:43:48 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,21 +6,12 @@
         {
         }
 
-        internal class ExceptionExample
+        public static void Import(string path)
         {
-            public static void Run()
+            if (string.IsNullOrWhiteSpace(path))
             {
-                try
-                {
-                    DoWork();
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
+                throw new Exception("Path is required.");
             }
-
-            private static void DoWork() => throw new InvalidOperationException();
         }
     }
 }
