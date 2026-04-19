8ceb1d4f9b14d702c386d41e329ee4afe58d1600
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-04-19 19:33:06 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,28 +1,26 @@
-﻿using Microsoft.Data.SqlClient;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            UnsafeQuery("test", "test", "test");
         }
 
-        public static object UnsafeQuery(string connection, string name, string password)
+        internal class ExceptionExample
         {
-            SqlConnection someConnection = new SqlConnection(connection);
-            SqlCommand someCommand = new SqlCommand();
-            someCommand.Connection = someConnection;
+            public static void Run()
+            {
+                try
+                {
+                    DoWork();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
 
-            someCommand.CommandText = "SELECT AccountNumber FROM Users " +
-               "WHERE Username='" + name +
-               "' AND Password='" + password + "'";
-
-            someConnection.Open();
-            object accountNumber = someCommand.ExecuteScalar();
-            someConnection.Close();
-            return accountNumber;
+            private static void DoWork() => throw new InvalidOperationException();
         }
     }
 }
