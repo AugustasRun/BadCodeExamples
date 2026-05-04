353ca653fd5798a0823f5fb0a3730ea83c76caa7
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-05-04 18:24:49 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,19 +1,20 @@
-﻿using Microsoft.Win32;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     internal sealed class Program
     {
         static async Task Main(string[] args)
         {
+            var temp = new C();
         }
+    }
 
-        public static string? ReadRegistry()
+    internal sealed class C
+    {
+        public static void M(IReadOnlyList<string> list)
         {
-            return Registry.GetValue(
-                @"HKEY_LOCAL_MACHINE\Software\MyApp",
-                "InstallPath",
-                null)?.ToString();
+            Console.Write(list.First());
+            Console.Write(list.Last());
+            Console.Write(list.Count());
         }
     }
 }
